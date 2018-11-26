035b13763c7293336225fa59a997ec6d5a3de283
Wong Cho Hing <venus.wong@oocl.com>
2018-11-27 02:23:40 +0800

    Refactor: function getPrice in class priceCalculator

---
+++ src/test/java/com/tws/refactoring/extract_method/PriceCalculatorTest.java
@@ -0,0 +1,32 @@
+package com.tws.refactoring.extract_method;
+
+import com.tws.refactoring.extract_variable.PriceCalculator;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class PriceCalculatorTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void priceCalculatorTest() {
+        PriceCalculator priceCalculator = new PriceCalculator();
+        double expectedValue = priceCalculator.getPrice(2, 4);
+    }
+}
