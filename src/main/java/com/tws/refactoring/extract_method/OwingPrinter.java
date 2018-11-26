8574921196a06e69b7d4477f6523a65188fe267f
Wong Cho Hing <venus.wong@oocl.com>
2018-11-27 01:12:51 +0800

    Refactor: class Order in Class extract_method, extract class to another java class

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -34,15 +34,3 @@ public class OwingPrinter {
 
     }
 }
-
-class Order {
-    private final double amount;
-
-    public Order(double amount) {
-        this.amount = amount;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-}
